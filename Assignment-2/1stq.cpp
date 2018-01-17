83b57b4bd62159be424d644005c9c87bb5980c90
Dheemanth Regati <dheemanthregati@gmail.com>
2018-01-17 16:03:57 +0530

    commit

--- Assignment-2/1stq.cpp
+++ Assignment-2/1stq.cpp
@@ -5,6 +5,11 @@ class node
 public:
 int data;
 node *next;
+
+node() {
+    data = 0;
+    next = nullptr;
+}
 };
 class linked_list
 {
@@ -18,6 +23,8 @@ tail = NULL;
 }
 void insert(int data);
 void display();
+int countItems();
+void Delete(int d);
 };
 void linked_list::insert (int data){
 node *tmp = new node;
@@ -36,19 +43,57 @@ tail = tail->next;
 cout << "Node with data " << data << " successfully added." << endl;
 }
 void linked_list::display (void){ 
-    node *tmp = new node; 
-    tmp->data = (head->data); 
-    tmp->next = (head->next); 
+    node *tmp = head; 
     while(tmp !=NULL){ 
         cout<<(tmp->data)<<" -> "; 
         tmp = tmp->next;
          } 
-         cout<<"NULL";
+         cout<<"NULL\n";
           }
+
+int linked_list::countItems (void){
+    node *tmp = head;
+    int c=0;
+    while(tmp !=NULL){
+        tmp = tmp->next;
+        c++;
+    }
+    return c;
+}
+
+void linked_list::Delete (int d){
+    if(d==1){
+        head = NULL;
+    }
+    else if(d==2){
+        head->next=NULL;
+    }
+    else {
+
+        node *i = head;
+        for ( ; i->next->next != NULL ; i = i->next);
+
+        i->next = nullptr;
+        tail = i;
+    }
+
+/*  int c=1;
+    while(c<(d-2)){
+        tmp = tmp->next;
+        c++;
+    }
+    tmp->next=tail;
+    tail->next = NULL;
+    }
+    */
+}
+
 int main(){
-linked_list ll1;
-ll1.insert(1);
-ll1.insert(2);
-ll1.insert(3);
-ll1.display();
+    linked_list ll1;
+    ll1.insert(1);
+    int y=ll1.countItems();
+    cout<<y<<endl;
+    ll1.display();
+    ll1.Delete(y);
+    ll1.display();
 }
