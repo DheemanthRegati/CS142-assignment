b5ed1b68f8717459cfe7b6eede4dd03d368b9c0e
Dheemanth Regati <dheemanthregati@gmail.com>
2018-03-26 22:37:59 +0530

    commit

---
+++ BinarySearchTree/binarytree.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+struct node                         
+
+{   int data;
+    struct node *left;
+    struct node *right;
+
+}*root;
+
+class BST{
+  public:
+  BST(){
+  root = NULL;}
+  
+  void insert(node *tree, int a){
+    node *temp = new node;
+    temp->data = a;
+    temp->right = NULL;              
+    temp->left = NULL;
+    if (root == NULL){
+      root=temp;
+    }
+    if (tree != NULL){               
+      if (temp->data > tree->data){
+        if (tree->right != NULL){
+          insert(tree->right,temp->data);}
+        else{
+          tree->right = temp;}
+      }
+      if (temp->data < tree->data){
+        if (tree->left != NULL){
+        insert(tree->left,temp->data);}
+        else{
+        tree->left = temp;}
+        
+      }
+      if (temp->data == tree->data){
+        cout << "node exists already";
+      }
+    }
+
+  }
+
+  void display(node *curr){        
+    if (root == NULL){
+      cout << "tree is empty"<<endl; 
+    }
+    if(curr != NULL){
+      display(curr->left);
+      cout << curr->data << " ";
+      display(curr->right);}
+
+  }
+  void bsearch(node *curr,int val){      
+    if (root != NULL){
+      if (root->data == val){
+        cout << val <<" present!" << endl;}
+      if (curr != NULL){
+        if (val > curr->data){
+          bsearch(curr->right,val);}
+        if (val < curr->data) {
+          bsearch(curr->left,val);}
+        if (val == curr->data){
+          cout << val << " present!"<< endl;}
+      }
+      else{
+        cout << val << " absent!" << endl;}
+    }
+  }
+
+};
+
+
+int main(){
+  BST bst;
+  bst.insert(root,10);
+  bst.insert(root,7);
+  bst.insert(root,3);
+  bst.insert(root,5);
+  bst.insert(root,11);
+  bst.display(root);
+  cout << endl;
+  bst.bsearch(root,1);
+  bst.bsearch(root,7);
+
+return 0;}
+
